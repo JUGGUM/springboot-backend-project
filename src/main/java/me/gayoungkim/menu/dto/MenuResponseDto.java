75143d3dafc2f0764b0c1c2fdce380f2118c27cd
JUGGUM <gayoung801@naver.com>
2023-07-11 22:58:03 +0900

    Update: MenuResponseDto id 추가

--- src/main/java/me/gayoungkim/menu/dto/MenuResponseDto.java
+++ src/main/java/me/gayoungkim/menu/dto/MenuResponseDto.java
@@ -5,12 +5,13 @@ import me.gayoungkim.menu.domain.Menu;
 
 @Getter
 public class MenuResponseDto {
+  private final Long id;
   private final String title;
   private final String link;
 
-  public MenuResponseDto(Menu menu){
+  public MenuResponseDto(Menu menu) {
+    this.id = menu.getId();
     this.title = menu.getTitle();
     this.link = menu.getLink();
   }
-
 }
