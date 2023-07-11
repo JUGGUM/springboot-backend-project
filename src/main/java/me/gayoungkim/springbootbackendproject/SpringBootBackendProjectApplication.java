044f1b43d6b9f3bf787ae2c90f0a163065866da6
JUGGUM <gayoung801@naver.com>
2023-07-11 13:56:56 +0900

    Feat: @SpringBootApplication 어노테이션 추가
    - SpringBoot 기본 설정 추가
    - 스프링 Bean 읽기와 생성을 자동설정

---
+++ src/main/java/me/gayoungkim/springbootbackendproject/SpringBootBackendProjectApplication.java
@@ -0,0 +1,11 @@
+package me.gayoungkim.springbootbackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendProjectApplication.class, args);
+    }
+}
