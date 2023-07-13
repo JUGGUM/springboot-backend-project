587ff22ea2eee8d28a4a9e23009ee596069f463e
JUGGUM <gayoung801@naver.com>
2023-07-13 19:41:18 +0900

    Update: MenuResponseDto subMenus 추가
    - MenuResponseDto 에 subMenu를 추가하여 상위메뉴조회시 하위메뉴도조회할수있도록합니다.

--- src/main/java/me/gayoungkim/menu/dto/MenuResponseDto.java
+++ src/main/java/me/gayoungkim/menu/dto/MenuResponseDto.java
@@ -1,5 +1,7 @@
 package me.gayoungkim.menu.dto;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import me.gayoungkim.menu.domain.Menu;
 
@@ -8,10 +10,12 @@ public class MenuResponseDto {
   private final Long id;
   private final String title;
   private final String link;
+  private List<Menu> subMenus = new ArrayList<>();
 
   public MenuResponseDto(Menu menu) {
     this.id = menu.getId();
     this.title = menu.getTitle();
     this.link = menu.getLink();
+    this.subMenus = menu.getSubMenus();
   }
 }
