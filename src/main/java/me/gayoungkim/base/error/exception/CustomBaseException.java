b2a1b4f215c1b4bbc6915d632f5303078e026e52
JUGGUM <gayoung801@naver.com>
2023-07-13 18:58:19 +0900

    Feat: 예외처리를 위한 최상위 CustomBaseException 추가
    - 해당 Exception은 RuntimeException을 상속합니다.

---
+++ src/main/java/me/gayoungkim/base/error/exception/CustomBaseException.java
@@ -0,0 +1,22 @@
+package me.gayoungkim.base.error.exception;
+
+import me.gayoungkim.base.error.ErrorCode;
+
+public class CustomBaseException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+
+  public CustomBaseException(String message, ErrorCode errorCode) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+
+  public CustomBaseException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+
+  public ErrorCode getErrorCode() {
+    return errorCode;
+  }
+}
