c889570c98d0c037e9689cb95efc740c0e652a16
JUGGUM <gayoung801@naver.com>
2023-07-13 16:36:36 +0900

    Feat: 하위메뉴를 위한 SubMenuRequestDto 생성

---
+++ src/main/java/me/gayoungkim/menu/dto/SubMenuRequestDto.java
@@ -0,0 +1,21 @@
+package me.gayoungkim.menu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.gayoungkim.menu.domain.Menu;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class SubMenuRequestDto {
+
+  private String title;
+  private String link;
+
+  public Menu toEntity() {
+    return Menu.builder()
+        .title(title)
+        .link(link).build();
+  }
+}
