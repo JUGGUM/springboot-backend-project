edf7576c0e6b3844fb796d57bcc0fadfb9162f65
JUGGUM <gayoung801@naver.com>
2023-07-15 22:49:36 +0900

    Update: MenuControllerTest

--- src/test/java/me/gayoungkim/menu/controller/MenuControllerTest.java
+++ src/test/java/me/gayoungkim/menu/controller/MenuControllerTest.java
@@ -1,12 +1,18 @@
 package me.gayoungkim.menu.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import java.security.Principal;
+import me.gayoungkim.base.error.ErrorCode;
 import me.gayoungkim.menu.dto.MenuRequestDto;
+import me.gayoungkim.menu.repository.MenuRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -16,6 +22,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 
 @SpringBootTest
@@ -27,6 +35,19 @@ class MenuControllerTest {
 
   @Autowired
   protected ObjectMapper objectMapper;
+
+  @Autowired
+  private WebApplicationContext context;
+
+  @Autowired
+  MenuRepository menuRepository;
+
+  @BeforeEach
+  public void mockMvcSetup() {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+        .build();
+    menuRepository.deleteAll();
+  }
 
   @DisplayName("addMenu : 메뉴를 추가할때 title이 null이면 실패한다.")
   @Test
@@ -76,5 +97,20 @@ class MenuControllerTest {
 
     // then
     result.andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("findArticle: 잘못된 HTTP 메서드로 아티클을 조회하려고 하면 조회에 실패한 다.")
+  @Test
+  public void invalidHttpMethod() throws Exception {
+    // given
+    final String url = "/api/menus/{id}";
+    // when
+    final ResultActions resultActions = mockMvc.perform(post(url, 1));
+    // then
+    resultActions
+        .andDo(print())
+        .andExpect(status().isMethodNotAllowed())
+        .andExpect(jsonPath("$.message").value(ErrorCode.METHOD_NOT_ALLOWED.
+            getMessage()));
   }
 }
